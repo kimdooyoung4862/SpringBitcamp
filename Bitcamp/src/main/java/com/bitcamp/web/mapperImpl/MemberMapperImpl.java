fadd210a538f8e75a8ce50fe9f098ba47a165543
kimdooyoung4862 <skyfol1005@naver.com>
2018-03-05 13:18:38 +0900

    2018-03-05

---
+++ Bitcamp/src/main/java/com/bitcamp/web/mapperImpl/MemberMapperImpl.java
@@ -0,0 +1,35 @@
+package com.bitcamp.web.mapperImpl;
+
+import org.springframework.stereotype.Repository;
+import com.bitcamp.web.command.Command;
+import com.bitcamp.web.command.ResultMap;
+import com.bitcamp.web.mapper.MemberMapper;
+
+@Repository
+public class MemberMapperImpl implements MemberMapper{
+
+	@Override
+	public void insertMember(Command cmd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ResultMap selectMemberById(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap login(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteMember(Command cmd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
